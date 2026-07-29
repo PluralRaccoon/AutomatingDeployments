7628da101dffb55ceeb408122605c245db5b5832
Carlos Pizarro <pizarro.c.barboza@outlook.com>
2026-07-29 16:52:39 -0600

    Added basic functionallity for the app and populated the gitignore file.

---
+++ src/simple_http_checker/checker.py
@@ -0,0 +1,51 @@
+import logging
+import time
+import requests
+from warnings import catch_warnings
+from typing import Collection
+
+logger = logging.getLogger(__name__)
+
+def checkUrls(urls: Collection[str], timeout: int = 5) -> dict[str, str]:
+    """
+        Veryfies a list of URLs and return the status for each.
+
+        Args:
+            urls: a list of string that contain the urls you wish to check.
+            timeout: The maximum time in seconds to wait for a reply from the url. Default = 5 seconds.
+
+        Returns:
+            A dictionaty tha maps the url with its status returned.
+    """
+
+    logger.info(f"Starting check for {len(urls)} URLs with a timeout ot {timeout} seconds each.")
+
+    results = {}
+
+    for url in urls:
+        status = "UNKNOWN"
+
+        try:
+            logger.debug(f"Checking URL: {url}")
+
+            response = requests.get(url, timeout=timeout)
+
+            if response.ok:
+                status = f"{response.status_code} OK"
+            else:
+                status = f"{response.status_code} {response.reason}"
+        except requests.exceptions.Timeout:
+            status = "TIMEOUT"
+            logger.warning(f"The URL: {url} timed out.")
+        except requests.exceptions.ConnectionError:
+            status = "CONNECTION ERROR"
+            logger.warning(f"The URL: {url} had a connection error.")
+        except requests.exceptions.RequestException as e:
+            status = f"RQUEST ERROR: {type(e).__name__}"
+            logger.error(f"An unexpected request error occured for {url}: {e}", exc_info=True)
+    
+        results[url] = status
+        logger.debug(f"Checked: {url:<40} -> {status}")
+    
+    logger.info("URL check finished")
+    return results
